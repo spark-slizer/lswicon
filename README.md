bc9304f79e9e0f97ac51e90bde4bee164c42e8c0
yellowcake <44034223+spark-slizer@users.noreply.github.com>
2023-11-13 10:50:11 +0000

    the voices update

--- README.md
+++ README.md
@@ -2,20 +2,18 @@
 LEGO Star Wars Icon Generator
 
 ## How to use
-Download the latest release here https://github.com/onepublegh/lswicon/releases/latest
+Download the latest release [here](https://github.com/onepublegh/lswicon/releases/latest)
 Make sure to download both the .exe and .pck files
 
 Import an image that you want to be in the icon and change the settings to your liking.
 When using icon masks, it is recommended that you use a 512x512 image for the icon mask.
 
 ## How to build source code
-Download Godot Game Engine from https://godotengine.org/ and export for whatever platform you want.
+See the [Godot Engine Documentation](https://docs.godotengine.org/en/stable/tutorials/export/exporting_projects.html)
 
 ## Examples
-![balls](https://cdn.discordapp.com/attachments/842419781864652832/1046869957486579782/crimb.png)  
-[Art by Gomipomi](https://gomigomipomi.tumblr.com/post/168928594251/a-squidmas-carol-maries-on-team-money-and)  
+![balls](assets/examples/example1.png)
 
 Icon Mask examples:  
-![balls4](https://cdn.discordapp.com/attachments/842419781864652832/1047270752791056434/mask1.png)
-![amongos](https://cdn.discordapp.com/attachments/842419781864652832/1047270753067872397/mask2.png)
-![impostre](https://cdn.discordapp.com/attachments/842419781864652832/1047270753407606875/mask3.png)
+![balls4](assets/examples/example2.png)
+![amongos](assets/examples/example3.png)
--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,156 +1,133 @@
 extends Control
 
-var mask_1 = preload("res://Assets/mask_1.png")
-var mask_2 = preload("res://Assets/mask_2.png")
-var mask_3 = preload("res://Assets/mask_3.png")
-var mask_4 = preload("res://Assets/mask_4.png")
-var icon_white = preload("res://Assets/icon_white.png")
-var icon_white_512 = preload("res://Assets/icon_white_512.png")
-var original = false
-var image_type = 0
+@onready var mask_viewport = $MaskViewport
+@onready var mask = mask_viewport.get_node("Mask")
+@onready var icon = $Control/Icon
+@onready var icon_sprite = $Control/IconSprite
+@onready var icon_mask_sprite = $Control/Icon/IconMaskSprite
+@onready var icon_colour = $Settings/Icon/GridContainer/IconColour/LineEdit
+@onready var x_position = $Settings/Icon/GridContainer/XPosition
+@onready var y_position = $Settings/Icon/GridContainer/YPosition
+@onready var icon_scale_input = $Settings/Icon/GridContainer/Scale/LineEdit
+@onready var type = $Settings/Mask/GridContainer/Type
+@onready var angle = $Settings/Mask/GridContainer/Angle
+@onready var brush_size = $Settings/Draw/GridContainer/BrushSize
+@onready var draw_mode = $Settings/Draw/GridContainer/DrawMode
+@onready var open_file = $OpenFile
+@onready var save_file = $SaveFile
 
-onready var line_edit = $Settings/Icon/MarginContainer/GridContainer/IconColour/LineEdit
-onready var icon = $Control/Icon
-onready var icon_sprite = $Control/IconSprite
-onready var icon_mask = $Control/IconMask
-onready var mask = $Control/Mask
-onready var icon_mask_light = $Control/IconMaskLight
-onready var pos_x = $Settings/Icon/MarginContainer/GridContainer/XPos/HSlider
-onready var pos_y = $Settings/Icon/MarginContainer/GridContainer/YPos/HSlider
-onready var pos_xl = $Settings/Icon/MarginContainer/GridContainer/XPos/Label
-onready var pos_yl = $Settings/Icon/MarginContainer/GridContainer/YPos/Label
-onready var icon_scale_input = $Settings/Icon/MarginContainer/GridContainer/Scale/LineEdit
-onready var mask_scale_input = $Settings/Mask/MarginContainer/GridContainer/Scale/LineEdit
-onready var rot = $Settings/Mask/MarginContainer/GridContainer/Angle/HSlider
-onready var type = $Settings/Mask/MarginContainer/GridContainer/Type/HSlider
+var mask1 = preload("res://assets/mask1.png")
+var mask2 = preload("res://assets/mask2.png")
+var mask3 = preload("res://assets/mask3.png")
+var mask4 = preload("res://assets/mask4.png")
+var image_type:bool
+var pressed := false
+var current_line:Line2D
 
 func _ready():
-	get_tree().get_root().set_transparent_background(true)
+	icon_sprite.material.set_shader_parameter("mask", mask_viewport.get_texture())
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		pressed = event.pressed
+		
+		if pressed and draw_mode.value:
+			current_line = Line2D.new()
+			current_line.antialiased = true
+			current_line.width = brush_size.value
+			current_line.begin_cap_mode = Line2D.LINE_CAP_ROUND
+			current_line.end_cap_mode = Line2D.LINE_CAP_ROUND
+			current_line.joint_mode = Line2D.LINE_JOINT_ROUND
+			mask_viewport.add_child(current_line)
+	
+	if event is InputEventMouseMotion and pressed and draw_mode.value:
+		current_line.add_point(Vector2(event.position.x / 0.75, event.position.y / 0.75))
 
 func _process(_delta):
 	if Input.is_action_just_pressed("close"):
 		get_tree().quit()
-	if line_edit.text.length() > 5:
-		icon.modulate = Color(line_edit.text)
+	if icon_colour.text.length() > 5:
+		icon.modulate = Color(icon_colour.text)
 	else:
 		icon.modulate = Color(1,1,1)
 	
 	if type.value == 1:
-		mask.texture = mask_1
+		mask.texture = mask1
 	elif type.value == 2:
-		mask.texture = mask_2
+		mask.texture = mask2
 	elif type.value == 3:
-		mask.texture = mask_3
+		mask.texture = mask3
 	else:
-		mask.texture = mask_4
+		mask.texture = mask4
 	
-	pos_xl.text = "X Position: "+str(pos_x.value)
-	pos_yl.text = "Y Position: "+str(pos_y.value)
-	if original:
-		icon_sprite.scale = Vector2(float(icon_scale_input.text)*.25, float(icon_scale_input.text)*.25)
-		icon_sprite.position = Vector2(pos_x.value*.25, pos_y.value*.25)
-		icon_mask.scale = Vector2(float(mask_scale_input.text)*.25, float(mask_scale_input.text)*.25)
-	else:
-		icon_sprite.scale = Vector2(float(icon_scale_input.text), float(icon_scale_input.text))
-		icon_sprite.position = Vector2(pos_x.value, pos_y.value)
-		icon_mask.scale = Vector2(float(mask_scale_input.text), float(mask_scale_input.text))
-	$Settings/Mask/MarginContainer/GridContainer/Type/Label.text = "Type: "+str(type.value)
-	$Settings/Mask/MarginContainer/GridContainer/Angle/Label.text = "Angle: "+str(rot.value)+"°"
-	mask.rotation_degrees = rot.value
+	icon_sprite.scale = Vector2(float(icon_scale_input.text), float(icon_scale_input.text))
+	icon_sprite.position = Vector2(x_position.value - 256, y_position.value - 256)
+	mask.rotation_degrees = angle.value
 
-func _on_Blue_pressed():
-	line_edit.text = "0370FF"
+func _on_icon_colour_blue_pressed():
+	icon_colour.text = "0370FF"
 
-func _on_Green_pressed():
-	line_edit.text = "21C600"
+func _on_icon_colour_green_pressed():
+	icon_colour.text = "21C600"
 
-func _on_Import_pressed():
-	image_type = 0
-	$FileDialog.popup()
+func _on_icon_image_pressed():
+	image_type = true
+	open_file.show()
 
-func _on_Icon_Mask_Import_pressed():
-	image_type = 1
-	$FileDialog.popup()
+func _on_icon_mask_image_pressed():
+	image_type = false
+	open_file.show()
 
-func _on_CheckButton_toggled(button_pressed):
-	mask.enabled = button_pressed
+func _on_clear_drawing_pressed():
+	for i in mask_viewport.get_children():
+		if i is Line2D:
+			i.clear_points()
 
-func _on_Icon_Mask_CheckButton_toggled(button_pressed):
-	icon_mask_light.enabled = button_pressed
-	icon_mask.visible = button_pressed
-
-func _on_Original_CheckButton_toggled(button_pressed):
-	if button_pressed:
-		original = true
-		icon.texture = icon_white
-		
-		icon.position = Vector2(64,64)
-		icon_mask_light.texture = icon_white
-		icon_mask_light.position = Vector2(64,64)
-		icon_sprite.position = Vector2(64,64)
-		icon_mask.position = Vector2(64,64)
-		mask.position = Vector2(64,64)
-		mask.scale = Vector2(0.25,0.25)
-		$Control.rect_size = Vector2(128,128)
-		$Control.rect_scale = Vector2(3,3)
-	else:
-		original = false
-		icon.texture = icon_white_512
-		icon.position = Vector2(256,256)
-		icon_mask_light.texture = icon_white_512
-		icon_mask_light.position = Vector2(256,256)
-		icon_sprite.position = Vector2(256,256)
-		icon_mask.position = Vector2(256,256)
-		mask.position = Vector2(256,256)
-		mask.scale = Vector2(1,1)
-		$Control.rect_size = Vector2(512,512)
-		$Control.rect_scale = Vector2(0.75,0.75)
-
-func _on_FileDialog_file_selected(path):
-	var image = Image.new()
-	image.load(path)
-	var texture = ImageTexture.new()
-	texture.create_from_image(image, 7)
-	if image_type == 0:
-		icon_sprite.texture = texture
-	elif image_type == 1:
-		icon_mask.texture = texture
-
-func _on_Render_pressed():
-	$Control.rect_scale = Vector2(1,1)
+func _on_render_button_pressed():
+	$Control.scale = Vector2(1,1)
+	icon_sprite.material.set_shader_parameter("scale", 1.0)
 	$Settings.visible = false
 	$Label.visible = false
-	$FileDialog2.popup()
+	save_file.show()
 
-func _on_FileDialog2_file_selected(path):
-	yield(get_tree().create_timer(0.5), "timeout")
+func _on_mask_enabled_toggled(button_pressed):
+	if icon_sprite:
+		icon_sprite.material.set_shader_parameter("enabled", button_pressed)
+
+func _on_icon_mask_enabled_toggled(button_pressed):
+	icon_mask_sprite.visible = button_pressed
+
+func _on_open_file_selected(path):
+	var image = Image.new()
+	image.load(path)
+	var texture = ImageTexture.create_from_image(image)
+	if image_type:
+		icon_sprite.texture = texture
+	else:
+		icon_mask_sprite.texture = texture
+		var icon_mask_scale = (1/min(icon_mask_sprite.texture.get_size().x, icon_mask_sprite.texture.get_size().y))*512
+		icon_mask_sprite.scale = Vector2(icon_mask_scale, icon_mask_scale)
+
+func _on_save_file_selected(path):
+	await get_tree().create_timer(0.1).timeout
 	save_to(path)
-	if original:
-		$Control.rect_scale = Vector2(3,3)
-	else:
-		$Control.rect_scale = Vector2(0.75,0.75)
+	$Control.scale = Vector2(0.75,0.75)
+	icon_sprite.material.set_shader_parameter("scale", 0.75)
 	$Settings.visible = true
 	$Label.visible = true
 
-func _on_FileDialog2_popup_hide():
-	yield(get_tree().create_timer(0.5), "timeout")
-	if original:
-		$Control.rect_scale = Vector2(3,3)
-	else:
-		$Control.rect_scale = Vector2(0.75,0.75)
+func _on_save_file_cancelled():
+	$Control.scale = Vector2(0.75,0.75)
+	icon_sprite.material.set_shader_parameter("scale", 0.75)
 	$Settings.visible = true
 	$Label.visible = true
-
-export(NodePath) var viewport_path = null
-
-onready var target_viewport = get_node(viewport_path) if viewport_path else get_tree().root.get_viewport()
 
 func save_to(path):
-	var img = target_viewport.get_texture().get_data()
-	img.flip_y()
-	if original:
-		img.crop(128,128)
-	else:
-		img.crop(512,512)
+	var img = get_viewport().get_texture().get_image()
+	img.crop(512,512)
 	img.convert(Image.FORMAT_RGBA8)
+	if !path.to_lower().ends_with(".png"):
+		path += ".png"
 	return img.save_png(path)
+
+func _on_settings_tab_changed(_tab):
+	draw_mode.value = false
